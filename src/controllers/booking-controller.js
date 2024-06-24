f46469d29dfd28fa764c85af5e20b231600cbdb5
Vishal Kumar <102590210+vishalkumar-30@users.noreply.github.com>
2024-06-24 16:53:14 +0530

    Added rabbit mq config

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -2,7 +2,7 @@ const { StatusCodes } = require('http-status-codes');
 
 const { BookingService } = require('../services/index');
 
-// const { createChannel, publishMessage } = require('../utils/messageQueue');
+const { createChannel, publishMessage } = require('../utils/messageQueue');
 const { REMINDER_BINDING_KEY } = require('../config/serverConfig');
 
 const bookingService = new BookingService();
@@ -12,7 +12,14 @@ class BookingController {
     constructor() {
     }
 
-    
+    async sendMessageToQueue(req, res){
+        const channel = await createChannel();
+        const data = {message: 'Success'};
+        publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(data));
+        return res.status(200).json({
+            message: 'Succesfully published the event'
+        });
+    }
 
     async create (req, res) {
         try {
